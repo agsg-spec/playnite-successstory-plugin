3fec14eb26897d06706e8c0984fd1773e573a6a5
agsg-spec <ahmd0907555@gmail.com>
2025-12-06 04:08:09 +0300

    Delete source directory

--- source/Models/CbData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuccessStory.Models
-{
-    public class CbData
-    {
-        public string Name { get; set; }
-        public string Slug { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
