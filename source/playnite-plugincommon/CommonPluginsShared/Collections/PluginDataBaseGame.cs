1293755d777c9c0501d83230f03e91bec966bdf6
agsg-spec <ahmd0907555@gmail.com>
2025-12-06 04:46:54 +0300

    Add files via upload

---
+++ source/playnite-plugincommon/CommonPluginsShared/Collections/PluginDataBaseGame.cs
@@ -0,0 +1,20 @@
+﻿using Playnite.SDK.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonPluginsShared.Collections
+{
+    public abstract class PluginDataBaseGame<T> : PluginDataBaseGameBase
+    {
+        public abstract List<T> Items { get; set; }
+
+
+        [DontSerialize]
+        public override bool HasData => Items?.Count > 0;
+
+        [DontSerialize]
+        public override int Count => Items?.Count ?? 0;
+    }
+}
---
+++ source/playnite-plugincommon/CommonPluginsShared/Collections/PluginDataBaseGameBase.cs
@@ -0,0 +1,79 @@
+﻿using Playnite.SDK.Models;
+using Playnite.SDK.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonPluginsShared.Collections
+{
+    public class PluginDataBaseGameBase : DatabaseObject
+    {
+        #region Data
+        public DateTime DateLastRefresh { get; set; } = default;
+
+
+        [DontSerialize]
+        public bool IsDeleted { get; set; }
+
+        [DontSerialize]
+        public bool IsSaved { get; set; }
+
+
+        [DontSerialize]
+        public virtual bool HasData => false;
+
+        [DontSerialize]
+        public virtual int Count => 0;
+        #endregion
+
+        #region Game data
+        [DontSerialize]
+        internal Game Game { get; set; }
+
+
+        [DontSerialize]
+        public Guid SourceId => Game?.SourceId ?? default;
+
+        [DontSerialize]
+        public DateTime? LastActivity => Game?.LastActivity ?? null;
+
+        [DontSerialize]
+        public bool Hidden => Game?.Hidden ?? default;
+
+        [DontSerialize]
+        public string Icon => Game?.Icon ?? string.Empty;
+
+        [DontSerialize]
+        public string CoverImage => Game?.CoverImage ?? string.Empty;
+
+        [DontSerialize]
+        public string BackgroundImage => Game?.BackgroundImage ?? string.Empty;
+
+        [DontSerialize]
+        public List<Genre> Genres => Game?.Genres ?? default;
+
+        [DontSerialize]
+        public List<Tag> Tags => Game?.Tags ?? default;
+
+        [DontSerialize]
+        public List<Guid> GenreIds => Game?.GenreIds ?? default;
+
+        [DontSerialize]
+        public List<Platform> Platforms => Game?.Platforms ?? default;
+
+        [DontSerialize]
+        public ulong Playtime => Game?.Playtime ?? default;
+
+        [DontSerialize]
+        public bool Favorite => Game?.Favorite ?? default;
+
+        [DontSerialize]
+        public GameSource Source => Game?.Source ?? default;
+
+
+        [DontSerialize]
+        public bool IsInstalled => Game?.IsInstalled ?? default;
+        #endregion
+    }
+}
